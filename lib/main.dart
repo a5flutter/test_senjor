e374083804daa06c8d74fd5915d6d1cbba15ae98
a5flutter <5cbe47cb414b335d67e3685bc59808da92c38158>
2021-02-01 12:27:37 +0200

    bottom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_app/screens/bottom_widget.dart';
+import 'package:test_app/ui/screens/boost_your_links.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,7 +26,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BottomWidget() // This trailing comma makes auto-formatting nicer for build methods.
+      body: BoostYourLinks() // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

bed7c87ddc12017192f1ff399d5f1c361415c16a
a5flutter <5cbe47cb414b335d67e3685bc59808da92c38158>
2021-02-01 11:38:01 +0200

    bottom widget

---
+++ lib/theme/text_style/text_style.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/theme/colors/colors.dart';
+
+const TextStyle white18Poppins400 = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
+    fontFamily: 'Poppins',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w500,
+    height: 1.2
+);
+
+const TextStyle gray18Poppins400 = TextStyle(
+    color: gray,
+    fontSize: 18,
+    fontFamily: 'Poppins',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w500,
+    height: 1.2
+);
+
+const TextStyle white32Poppins400 = TextStyle(
+    color: Colors.white,
+    fontSize: 32,
+    fontFamily: 'Poppins',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+    height: 1.2
+);
